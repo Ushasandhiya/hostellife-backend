6298f4e615c72cea38e8d8baed8b9b6566ce3a7f
Ushasandhiya <sandhiya.pcy@gmail.com>
2026-01-07 18:47:31 +0530

    Set up backend with Express server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// middleware
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("🚀 HostelLife backend is running!");
+});
+
+// start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
